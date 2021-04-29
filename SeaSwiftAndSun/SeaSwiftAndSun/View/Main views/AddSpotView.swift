e043adca4a77da9b51056391a0d842e9587ca764
Amandine Cousin <cousin.amandine.pro@gmail.com>
2021-04-29 19:08:55 +0200

    Add image picker but don't send it to airtable

--- SeaSwiftAndSun/SeaSwiftAndSun/View/Main views/AddSpotView.swift
+++ SeaSwiftAndSun/SeaSwiftAndSun/View/Main views/AddSpotView.swift
@@ -11,6 +11,9 @@ struct AddSpotView: View {
     @State var spotName: String = ""
     @State var spotLocation: String = ""
     @State var spotBreak: SurfBreak = SurfBreak.beach
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+    
     @ObservedObject var viewModel = SpotViewModel()
     
     var body: some View {
@@ -29,11 +32,28 @@ struct AddSpotView: View {
 //                    Text($0.rawValue)
 //                }
 //            }
+            Image(uiImage: self.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 150)
+                            .edgesIgnoringSafeArea(.all)
+            Button(action: {
+                self.isShowPhotoLibrary = true
+            }) {
+                Text("Photo library")
+                    .font(.headline)
+                    .foregroundColor(Color("customOrange"))
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color("customOrange"), lineWidth: 2)
+                    )
+            }
             Button("Add") {
                 self.viewModel.createSpot(name: self.spotName, location: self.spotLocation, surfBreak: self.spotBreak)
             }
             .font(.title3)
-            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 20)
             .padding()
             .background(Color("customOrange"))
             .accentColor(.white)
@@ -43,6 +63,9 @@ struct AddSpotView: View {
                 .stroke(Color.white, lineWidth: 5))
         }
         .padding()
+        .sheet(isPresented: $isShowPhotoLibrary) {
+                    ImagePicker(sourceType: .photoLibrary, selectedImage: self.$image)
+        }
     }
 }
 
